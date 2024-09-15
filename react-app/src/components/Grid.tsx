3b61129db1f349370ff5d115c1cf110107388a08
Marcel Zielinski <marziel2001@gmail.com>
2024-09-15 19:33:17 +0200

    [React][GridPage] Refactor by adding Button window and button component

--- react-app/src/components/Grid.tsx
+++ react-app/src/components/Grid.tsx
@@ -2,12 +2,42 @@ import { useState } from "react";
 import styles from "../styles/GridPage.module.css";
 import { colors } from "../static/GridElements";
 import GridConfigButton from "./UI/GridButton";
+import GridButtonWindow from "./GridButtonWindow";
 
 export default function Grid() {
-  const getRandomColor = () => {
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    return colors[randomIndex];
+  const generateElements = () => {
+    const color: Array<Array<number>> = [];
+
+    for (let i = 0; i < numberOfElements; i++) {
+      color.push(getRandomColor());
+    }
+
+    return Array.from({ length: numberOfElements }).map((_, index) => (
+      <div
+        key={index}
+        className={`${styles["element"]} ${getRandomAspect()}`}
+        style={{
+          backgroundColor: "rgb(" + color[index].join(", ") + ")",
+          borderColor:
+            "rgb(" +
+            subtractWithSaturation8bit(60, color[index][0]).toString() +
+            ", " +
+            subtractWithSaturation8bit(60, color[index][1]).toString() +
+            ", " +
+            subtractWithSaturation8bit(60, color[index][2]).toString() +
+            ")",
+          textAlign: "center",
+          alignContent: "center",
+          fontSize: "2rem",
+          color: "black",
+        }}
+      >
+        <span>{index}</span>
+      </div>
+    ));
   };
+
+  const [isDense, setDense] = useState<boolean>(false);
 
   const [selectedAspects, setSelectedAspects] = useState([
     { id: 1, aspect: "aspect_1_to_2", selected: true },
@@ -16,6 +46,15 @@ export default function Grid() {
     { id: 4, aspect: "aspect_3_to_1", selected: true },
     { id: 5, aspect: "aspect_1_to_3", selected: true },
   ]);
+
+  const [elements, setElements] = useState(generateElements);
+
+  const numberOfElements = 20;
+
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
 
   function selectAspectID(id: number) {
     const newAspects = [...selectedAspects];
@@ -59,69 +98,26 @@ export default function Grid() {
     const randomIndex = Math.floor(Math.random() * aspects.length);
     return styles[aspects[randomIndex].aspect];
   };
-  const [isDense, setDense] = useState<boolean>(false);
 
-  const numberOfElements = 20;
-
-  const generateElements = () => {
-    const color: Array<Array<number>> = [];
-
-    for (let i = 0; i < numberOfElements; i++) {
-      color.push(getRandomColor());
-    }
-
-    return Array.from({ length: numberOfElements }).map((_, index) => (
-      <div
-        key={index}
-        className={`${styles["element"]} ${getRandomAspect()}`}
-        style={{
-          backgroundColor: "rgb(" + color[index].join(", ") + ")",
-          borderColor:
-            "rgb(" +
-            subtractWithSaturation8bit(60, color[index][0]).toString() +
-            ", " +
-            subtractWithSaturation8bit(60, color[index][1]).toString() +
-            ", " +
-            subtractWithSaturation8bit(60, color[index][2]).toString() +
-            ")",
-          textAlign: "center",
-          alignContent: "center",
-          fontSize: "2rem",
-          color: "black",
-        }}
-      >
-        <span>{index}</span>
-      </div>
-    ));
-  };
-
-  const [elements, setElements] = useState(generateElements);
 
   return (
     <div style={{ width: "100%", backgroundColor: "inherit" }}>
       <div className={styles["windows-container"]}>
-        <div className={styles["v-btn-window"]}>
-          <span className={styles["title"]}>Aspects</span>
-          <div className={styles["v-btn-cont"]}>{aspectButtons}</div>
-        </div>
-
-        <div className={styles["v-btn-window"]}>
-          <span className={styles["title"]}>Grid options</span>
-          <div className={styles["v-btn-cont"]}>
-            <GridConfigButton
-              name="Generate"
-              handleClick={() => {
-                setElements(generateElements);
-              }}
-            />
-            <GridConfigButton
-              name={isDense ? "dense" : "row"}
-              handleClick={() => {
-                setDense(!isDense);
-              }}
-            />
-          </div>
-        </div>
+        <GridButtonWindow title="Aspects">{aspectButtons}</GridButtonWindow>
+        <GridButtonWindow title="Grid options">
+          <GridConfigButton
+            name="Generate"
+            handleClick={() => {
+              setElements(generateElements);
+            }}
+          />
+          <GridConfigButton
+            name={isDense ? "dense" : "row"}
+            handleClick={() => {
+              setDense(!isDense);
+            }}
+          />
+        </GridButtonWindow>
       </div>
 
       <div
