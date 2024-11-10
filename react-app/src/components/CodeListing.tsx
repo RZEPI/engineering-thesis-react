277fdab6208c3ec56dd4346b4924f0ae652a00a8
Marcel Zielinski <marziel2001@gmail.com>
2024-11-10 15:45:10 +0100

    [React][GridPage] Fix buttons props types and passing

--- react-app/src/components/CodeListing.tsx
+++ react-app/src/components/CodeListing.tsx
@@ -7,21 +7,19 @@ export function CodeListing({ cssProps }: { cssProps: CSSProperties }) {
       &#123;
       <br />
       <span style={{ paddingLeft: "2em" }}>
-        {" "}
-        grid-auto-flow : {cssProps.gridAutoFlow}{" "}
+        grid-auto-flow: {cssProps.gridAutoFlow};
       </span>
       <br />
       <span style={{ paddingLeft: "2em" }}>
-        {" "}
-        grid-template-columns : {cssProps.gridTemplateColumns}{" "}
+        grid-template-columns: {cssProps.gridTemplateColumns};
       </span>
       <br />
       <span style={{ paddingLeft: "2em" }}>
-        {" "}
-        grid-auto-rows : {cssProps.gridAutoRows}{" "}
+        grid-auto-rows: {cssProps.gridAutoRows}
+        {";"}
       </span>
       <br />
-      <span style={{ paddingLeft: "2em" }}> gap : {cssProps.gap} </span>
+      <span style={{ paddingLeft: "2em" }}>gap: {cssProps.gap};</span>
       <br />
       &#125;
     </div>
