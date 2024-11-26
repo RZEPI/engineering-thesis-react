c268c0dc734d3b9986f8b864bf14539329b41bf6
Marcel Zielinski <marziel2001@gmail.com>
2024-11-26 01:27:18 +0100

    [React][GridPage] Make grid element a component

--- react-app/src/components/GridPage.tsx
+++ react-app/src/components/GridPage.tsx
@@ -8,6 +8,7 @@ import { subtractWithSaturation8bit } from "../utils.tsx";
 import { CodeListing } from "./CodeListing.tsx";
 import Grid from "./Grid.tsx";
 import { AspectStates } from "../models/AspectsState.ts";
+import GridElement from "./UI/GridElement.tsx";
 
 export default function GridPage() {
   const numberOfElements = 20;
@@ -20,11 +21,36 @@ export default function GridPage() {
   });
 
   const [selectedAspects, setSelectedAspects] = useState<AspectStates>([
-    { id: 1, aspectClass: "aspect_1_to_2", aspectText: "1 to 2", selected: true },
-    { id: 2, aspectClass: "aspect_2_to_1", aspectText: "2 to 1", selected: true },
-    { id: 3, aspectClass: "aspect_1_to_1", aspectText: "1 to 1", selected: true },
-    { id: 4, aspectClass: "aspect_3_to_1", aspectText: "3 to 1", selected: true },
-    { id: 5, aspectClass: "aspect_1_to_3", aspectText: "1 to 3", selected: true },
+    {
+      id: 1,
+      aspectClass: "aspect_1_to_2",
+      aspectText: "1 to 2",
+      selected: true,
+    },
+    {
+      id: 2,
+      aspectClass: "aspect_2_to_1",
+      aspectText: "2 to 1",
+      selected: true,
+    },
+    {
+      id: 3,
+      aspectClass: "aspect_1_to_1",
+      aspectText: "1 to 1",
+      selected: true,
+    },
+    {
+      id: 4,
+      aspectClass: "aspect_3_to_1",
+      aspectText: "3 to 1",
+      selected: true,
+    },
+    {
+      id: 5,
+      aspectClass: "aspect_1_to_3",
+      aspectText: "1 to 3",
+      selected: true,
+    },
   ]);
 
   const [elements, setElements] = useState(getGeneratedElements);
@@ -67,28 +93,11 @@ export default function GridPage() {
     }
 
     return Array.from({ length: numberOfElements }).map((_, index) => (
-      <div
-        key={index}
-        className={`${styles["element"]} ${getRandomAspect()}`}
-        style={{
-          backgroundColor: "rgb(" + colors[index].join(", ") + ")",
-          borderColor:
-            "rgb(" +
-            subtractWithSaturation8bit(60, colors[index][0]).toString() +
-            ", " +
-            subtractWithSaturation8bit(60, colors[index][1]).toString() +
-            ", " +
-            subtractWithSaturation8bit(60, colors[index][2]).toString() +
-            ")",
-          textAlign: "center",
-          alignContent: "center",
-          fontSize: "2rem",
-          color: "black",
-          overflow: "hidden",
-        }}
-      >
-        <span>{index}</span>
-      </div>
+      <GridElement
+        aspect={getRandomAspect()}
+        color={colors[index]}
+        index={index}
+      ></GridElement>
     ));
   }
 
