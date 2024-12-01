6b3bc96db586f88eafdd2a56a5ceace5d199c557
Marcel Zielinski <marziel2001@gmail.com>
2024-12-01 21:28:03 +0100

    [React][GridPage] Merge Utils.ts

--- react-app/src/util/util.ts
+++ react-app/src/util/util.ts
@@ -4,3 +4,8 @@ export function drawColor() {
   const b = Math.floor(Math.random() * 256);
   return `rgb(${r}, ${g}, ${b})`;
 }
+
+export function subtractWithSaturation8bit(amount: number, value: number) {
+  const retVal = value - amount < 0 ? 0 : value - amount;
+  return retVal;
+}
