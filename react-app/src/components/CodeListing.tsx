fb88d1abff0364cf2a7d0fd502528e1f45f11fd4
Marcel Zielinski <marziel2001@gmail.com>
2024-12-02 22:55:43 +0100

    [React][GridPage] Change codelisting to be flexible and not hardcoded

--- react-app/src/components/CodeListing.tsx
+++ react-app/src/components/CodeListing.tsx
@@ -2,24 +2,41 @@ import { CSSProperties } from "react";
 import styles from "../styles/GridPage.module.css";
 
 export function CodeListing({ cssProps }: { cssProps: CSSProperties }) {
+
+function generateEntries() {
+  return parsedEntries.map((e, index) => (
+    <div key={index}>
+      <span style={{ paddingLeft: "2em" }}>
+        {e.propertyKey}: {e.propertyValue}
+      </span>
+      {index < parsedEntries.length - 1 && <br />}
+    </div>
+  ));
+}
+
+const parsedEntries: { propertyKey: string; propertyValue: string }[] = [];
+
+  for (const [propertyKey, propertyValue] of Object.entries(cssProps)) {
+    let upperCaseCharIdx = null;
+    let idx = 0;
+    for (const character of propertyKey) {
+      const code = character.charCodeAt(0);
+      if (code >= 65 && code <= 90) upperCaseCharIdx = idx;
+      idx++;
+    }
+    let parsedKey;
+    if (upperCaseCharIdx)
+      parsedKey =
+        `${propertyKey.substring(0, upperCaseCharIdx)}-${propertyKey.substring(upperCaseCharIdx)}`.toLowerCase();
+    else parsedKey = propertyKey;
+
+    parsedEntries.unshift({ propertyKey: parsedKey, propertyValue });
+  }
+
   return (
     <div className={styles["code-listing"]}>
       &#123;
-      <br />
-      <span style={{ paddingLeft: "2em" }}>
-        grid-auto-flow: {cssProps.gridAutoFlow};
-      </span>
-      <br />
-      <span style={{ paddingLeft: "2em" }}>
-        grid-template-columns: {cssProps.gridTemplateColumns};
-      </span>
-      <br />
-      <span style={{ paddingLeft: "2em" }}>
-        grid-auto-rows: {cssProps.gridAutoRows};
-      </span>
-      <br />
-      <span style={{ paddingLeft: "2em" }}>gap: {cssProps.gap};</span>
-      <br />
+        {generateEntries()}
       &#125;
     </div>
   );
