feee8ac556a11e276e52bf52611be16be3f17ff0
Marcel Zielinski <marziel2001@gmail.com>
2024-12-03 18:39:50 +0100

    [React][GridPage] Fix css entry parser working only for 2 word entries

--- react-app/src/components/CodeListing.tsx
+++ react-app/src/components/CodeListing.tsx
@@ -1,42 +1,27 @@
 import { CSSProperties } from "react";
 import styles from "../styles/GridPage.module.css";
+import { parseCssEntries } from "../util/util";
 
 export function CodeListing({ cssProps }: { cssProps: CSSProperties }) {
 
-function generateEntries() {
-  return parsedEntries.map((e, index) => (
-    <div key={index}>
-      <span style={{ paddingLeft: "2em" }}>
-        {e.propertyKey}: {e.propertyValue}
-      </span>
-      {index < parsedEntries.length - 1 && <br />}
-    </div>
-  ));
-}
-
-const parsedEntries: { propertyKey: string; propertyValue: string }[] = [];
-
-  for (const [propertyKey, propertyValue] of Object.entries(cssProps)) {
-    let upperCaseCharIdx = null;
-    let idx = 0;
-    for (const character of propertyKey) {
-      const code = character.charCodeAt(0);
-      if (code >= 65 && code <= 90) upperCaseCharIdx = idx;
-      idx++;
-    }
-    let parsedKey;
-    if (upperCaseCharIdx)
-      parsedKey =
-        `${propertyKey.substring(0, upperCaseCharIdx)}-${propertyKey.substring(upperCaseCharIdx)}`.toLowerCase();
-    else parsedKey = propertyKey;
-
-    parsedEntries.unshift({ propertyKey: parsedKey, propertyValue });
+  function generateEntries() {
+    return parsedEntries.map((e, index) => (
+      <div key={index}>
+        <span style={{ paddingLeft: "2em" }}>
+          {e.propertyKey}: {e.propertyValue}
+        </span>
+        {index < parsedEntries.length - 1 && <br />}
+      </div>
+    ));
   }
+
+  const parsedEntries: { propertyKey: string; propertyValue: string }[] =
+    parseCssEntries(cssProps);
 
   return (
     <div className={styles["code-listing"]}>
       &#123;
-        {generateEntries()}
+      {generateEntries()}
       &#125;
     </div>
   );
