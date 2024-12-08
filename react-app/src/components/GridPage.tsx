b0450c8442fb3fcfacabe460f9ba3de38d39b704
Marcel Zielinski <marziel2001@gmail.com>
2024-12-08 22:03:22 +0100

    [React][GridPage] Fix slider function not to be inlined

--- react-app/src/components/GridPage.tsx
+++ react-app/src/components/GridPage.tsx
@@ -113,36 +113,27 @@ export default function GridPage() {
     setSelectedAspects(newAspects);
   }
 
+  function changeGridAutoFlow() {
+    const newProps: CSSProperties = {};
+    newProps.gridAutoFlow = cssProps.gridAutoFlow == "dense" ? "row" : "dense";
+    setGridCss(newProps);
+  }
+
   return (
     <div className={styles["page-wrapper"]}>
       <div className={styles["page-content"]}>
         <div className={styles["windows-container"]}>
           <GridButtonWindow title="Aspects">{aspectButtons}</GridButtonWindow>
           <GridButtonWindow title="Grid options">
-            <GridConfigButton
-              name="Generate"
-              handleClick={() => {
-                setElements(generateElements);
-              }}
-            />
-            <GridConfigButton
-              name={cssProps.gridAutoFlow as string}
-              handleClick={() => {
-                const newProps: CSSProperties = {};
-                newProps.gridAutoFlow =
-                  cssProps.gridAutoFlow == "dense" ? "row" : "dense";
-                setGridCss(newProps);
-              }}
-            />
+            <GridConfigButton name="Generate" handleClick={() => {setElements(generateElements);}}/>
+            <GridConfigButton name={cssProps.gridAutoFlow as string} handleClick={changeGridAutoFlow} />
             <GridConfigSlider
               defaultValue={cssProps.gridTemplateColumns?.valueOf()}
-              name="Element size"
-              handleOnChange={setElementSizeHandler}
+              name="Element size" handleOnChange={setElementSizeHandler}
             ></GridConfigSlider>
             <GridConfigSlider
               defaultValue={cssProps.gap?.valueOf()}
-              name="Gap size"
-              handleOnChange={setGapSizeHandler}
+              name="Gap size" handleOnChange={setGapSizeHandler}
             ></GridConfigSlider>
           </GridButtonWindow>
         </div>
